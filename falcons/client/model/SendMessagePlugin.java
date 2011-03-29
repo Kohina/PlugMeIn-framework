c7d74ae01ea1c221437851185883cb2c55fc38ed
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-03-29 12:17:03 +0000

    Created a few checks in the SendMessagePlugin, to see if the information sent from the server is something that the SendMessagePlugin can handle.

--- falcons/client/model/SendMessagePlugin.java
+++ falcons/client/model/SendMessagePlugin.java
@@ -9,6 +9,9 @@ import falcons.client.network.PluginCall;
 //This is not supposed to exist here. TODO
 public class SendMessagePlugin extends AbstractPlugin {
 	
+	private String pluginID = "SendMessagePlugin";
+	private String versionID = "0.1";
+	
 	/**
 	 * The PluginData associated with the plugin
 	 * @author Printz
@@ -39,10 +42,26 @@ public class SendMessagePlugin extends AbstractPlugin {
 	public void receiveCall(PluginCall call) {
 		AbstractPluginData data = call.getPluginData();
 		
-		if(data.getMethodID().equals("SendMessage")){
+		if(data.getMethodID().equals("SendMessage")&&data.getVersionID().equals(versionID)){
 			JOptionPane.showMessageDialog(null, ((SendMessagePluginData)data).getMessage());
+		} else if(!data.getMethodID().equals("SendMessage")&&!data.getVersionID().equals(versionID)){
+			System.out.println("Both the methodID and the versionID is not the same as the plugin sending the call.");
+		} else if(!data.getMethodID().equals("SendMessage")){
+			System.out.println("There doesn't exist a method with that methodID.");
+		} else if(!data.getVersionID().equals(versionID)){
+			System.out.println("The version of the plugin sending the PluginCall is not the same as the one receiving it.");
 		}
 
+	}
+
+	@Override
+	public String getPluginID() {
+		return pluginID;
+	}
+
+	@Override
+	public String getVersionID() {
+		return versionID;
 	}
 
 }
