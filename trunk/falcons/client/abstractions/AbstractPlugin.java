37af3e7df84a28b01be839303ce29b3331d94733
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-03-29 12:17:03 +0000

    Created a few checks in the SendMessagePlugin, to see if the information sent from the server is something that the SendMessagePlugin can handle.

--- trunk/falcons/client/abstractions/AbstractPlugin.java
+++ trunk/falcons/client/abstractions/AbstractPlugin.java
@@ -8,27 +8,24 @@ public abstract class AbstractPlugin {
 
 	private String pluginID;
 	private String versionID;
+	
 
 	/**
 	 * 
 	 * @return Returns the ID of the plugin.
 	 */
-	public String getPluginID() {
-		return pluginID;
-	}
+	public abstract String getPluginID();
 
 	/**
 	 * 
 	 * @return Returns the ID of the plugins version.
 	 */
-	public String getVersionID() {
-		return versionID;
-	}
+	public abstract String getVersionID();
 
 	/**
 	 * Receives a call from the server and checks what it is that it is supposed
 	 * to do. Checks that the version of the plugin sending the call is the same
-	 * as the receiving one.
+	 * as the receiving one and that there actually exists a method with that ID.
 	 * 
 	 * @param call
 	 *            The PluginCall from the server.
