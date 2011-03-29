16be2353e7fd2fcc2120e7a8bdb78fbab2f2c75d
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-03-29 11:32:17 +0000

    Started implementing the client. Not enough done to actually run yet but the base is there.

--- falcons/client/network/CommunicationCenter.java
+++ falcons/client/network/CommunicationCenter.java
@@ -3,20 +3,26 @@ package falcons.client.network;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.ObjectStreamClass;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
 
 public class CommunicationCenter {
 
 	private Socket socket = null;
 	private DataInterpreter interpreter;
 	private ObjectInputStream in = null;
+	// TODO create an output.
 	private ObjectOutputStream out = null;
 	
-	public CommunicationCenter (DataInterpreter interpreter, String ip, int port){
+	/**
+	 * Contructor for the CommunicationCenter.
+	 * @param interpreter The DataInterpreter that's being used by the client.
+	 * @param ip The IP-address of the server 
+	 * @param port The port that the server uses to accept connections.
+	 * @throws IOException If an unhandled IOException is thrown then it could not find the I/O Connection for the socket.
+	 */
+	public CommunicationCenter (DataInterpreter interpreter, String ip, int port) throws IOException{
 		
 		try {
 			// Create an interpreter associated with the client
@@ -24,25 +30,28 @@ public class CommunicationCenter {
 			// Create a new socket
 			this.socket = new Socket(ip, port);
 		// Catch UnknownHostException and tell the user about it.
-		} catch (UnknownHostException) {
+		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host: " + ip);
-			System.exit(1);
 		// Catch IOException and tell the user about it.
 		} catch (IOException e) {
 			System.err.println("Couldn't get I/O for the connection to: " + ip);
-			System.exit(1);
 		}
+		
 		// Associate the input and output stream to ObjectStreams.
-		in = socket.getInputStream();
-		out = socket.getOutputStream();
+			in = (ObjectInputStream) socket.getInputStream();
+			out = (ObjectOutputStream) socket.getOutputStream();
 	}
-	
-	private void client(){
+	 /**
+	  * Start listening to the socket.
+	  * @throws IOException If an unhandled IOException is thrown then we lost connection to the inputstream.
+	  * @throws ClassNotFoundException If an unhandled ClassNotFoundException is thrown a disallowed object was sent to the client.
+	  */
+	private void client() throws IOException, ClassNotFoundException{
 		// The holder of the PluginCall received through the InputStream.
-		AbstractPluginCall call;
+		PluginCall call;
 		
 		// Always listen.
-		while(call = in.readObject() !=null){
+		while((call=(PluginCall) in.readObject()) !=null){
 			interpreter.interpret(call);
 		}
 	}
