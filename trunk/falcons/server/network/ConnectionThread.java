5af36d8cba91016d7df8ea1c66de2a6e2d7b7d35
jackpettersson@gmail.com <jackpettersson@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-03-30 13:37:40 +0000

    Continued work on the run method and wrote the send method.

--- trunk/falcons/server/network/ConnectionThread.java
+++ trunk/falcons/server/network/ConnectionThread.java
@@ -1,9 +1,13 @@
 package falcons.server.network;
 
+import java.net.*;
+import java.io.*;
 
 public class ConnectionThread extends Thread{
 
 	private ServerSocket socket = null;
+	ObjectInputStream in;
+	ObjectOutputStream out;
 	
 	/*
 	 *Contructor that sets the instace variable
@@ -16,8 +20,25 @@ public class ConnectionThread extends Thread{
 	@Override
 	public void run() {
 		Socket clientSocket = null;
-		while(){
-			clientSocket = socket.accept();
+		in = (ObjectInputStream) clientSocket.getInputStream();
+		out = (ObjectOutputStream) clientSocket.getOutputStream();
+		PluginCall call;
+		
+		clientSocket = socket.accept();
+		
+		while ((call = (PluginCall) in.readObject()) != null) {	
+			    interpreter.interpret(call);
 		}
+	}
+	
+	/**
+	 * Sends a PluginCall to the connected client. Takes the PluginCall as a parameter.
+	 * 
+	 * @param call
+	 * 				The PluginCall which should be sent to the client.
+	 * @throws IOException
+	 */
+	public void send(PluginCall call) throws IOException {
+		out.writeObject(call);
 	}
 }
