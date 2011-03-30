0c68c52860328838a97aecf97a805ba6918f1864
marre_kent@hotmail.com <marre_kent@hotmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-03-30 13:42:18 +0000

    GUI

---
+++ trunk/falcons/client/view/ClientFrame.java
@@ -0,0 +1,59 @@
+package falcons.client.view;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class ClientFrame extends javax.swing.JFrame {
+	private JTabbedPane jTabbedPaneOut;
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				ClientFrame inst = new ClientFrame();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public ClientFrame() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			{
+				jTabbedPane1 = new JTabbedPane();
+				getContentPane().add(jTabbedPane1, BorderLayout.CENTER);
+				jTabbedPane1.addTab("Main", arg1);
+				jTabbedPane1.addTab("Connection", component);
+			}
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+
+}
