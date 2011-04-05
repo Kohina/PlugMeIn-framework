c3df75b4d259062bbff9472ac86d4c1e066829df
marre_kent@hotmail.com <marre_kent@hotmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-04-05 20:02:22 +0000

    GUI

--- trunk/falcons/client/view/ClientView.java
+++ trunk/falcons/client/view/ClientView.java
@@ -23,7 +23,6 @@ public class ClientView extends javax.swing.JFrame {
 	private JTextField sendMessageTextField, IPTextField, temp, portTextField;
 	private JLabel portLabel, IPLabel, autoCon;
 	private JButton sendMessageButton, connectButton;
-	private JCheckBox autoConnect = new JCheckBox();
 
 	/**
 	* Auto-generated main method to display this JFrame
@@ -70,8 +69,8 @@ public class ClientView extends javax.swing.JFrame {
 	/**
 	 * Adds a new tab
 	 * 
-	 * @param s - A String containing pluginName
-	 * @param p - A JPanel with pluginContent
+	 * @param s - String containing pluginName
+	 * @param p - JPanel with pluginContent
 	 */
 	public void addTab(String s, JPanel p){
 		jTabbedPaneMain.addTab("<html><body marginwidth=5 marginheight=5>" + s + "</body></html>", p);
@@ -85,16 +84,6 @@ public class ClientView extends javax.swing.JFrame {
 			sendMessageButton.setText("Send");
 		}
 		return sendMessageButton;
-	}
-	
-	private JButton getConnectButton() {
-		if(connectButton == null) {
-			connectButton = new JButton();
-			connectButton.setSize(200, 100);
-			connectButton.setText("Connect");
-			connectButton.setPreferredSize(new java.awt.Dimension(59, 19));
-		}
-		return connectButton;
 	}
 	
 	/*TextFields*/
@@ -116,22 +105,6 @@ public class ClientView extends javax.swing.JFrame {
 		return sendMessageTextField;
 	}
 	
-	private JTextField getPortTextField() {
-		if(portTextField == null) {
-			portTextField = new JTextField();
-			portTextField.setPreferredSize(new java.awt.Dimension(50, 23));
-		}
-		return portTextField;
-	}
-
-	private JTextField getIPTextField() {
-		if(IPTextField == null) {
-			IPTextField = new JTextField();
-			IPTextField.setPreferredSize(new java.awt.Dimension(230, 23));
-		}
-		return IPTextField;
-	}
-	
 	/*Panels*/
 	private JPanel getWakeOnLanPanel(){
 		if(wakeOnLanPanel == null){
@@ -142,61 +115,6 @@ public class ClientView extends javax.swing.JFrame {
 		return wakeOnLanPanel;
 	}
 	
-	private JPanel getConnectionPanel() {
-		if(connectionPanel == null) {
-			connectionPanel = new JPanel(new GridBagLayout());
-			GridBagConstraints c = new GridBagConstraints();
-			
-			TitledBorder title = BorderFactory.createTitledBorder("Server connection");
-			connectionPanel.setBorder(title);
-			
-			c.gridx = 0;
-			c.gridy = 0;
-			connectionPanel.add(getIPLabel(), c);
-			c.gridx = 1;
-			c.gridy = 0;
-			c.gridwidth = 3;
-			connectionPanel.add(getIPTextField(), c);
-			c.gridx = 0;
-			c.gridy = 1;
-			c.gridwidth = 1;
-			connectionPanel.add(getPortLabel(), c);
-			c.gridx = 1;
-			c.gridy = 1;
-			connectionPanel.add(getPortTextField(), c);
-			c.gridx = 2;
-			c.gridy = 1;
-			connectionPanel.add(getAutoCon(), c);
-			c.gridx = 3;
-			c.gridy = 1;
-			connectionPanel.add(autoConnect, c);
-			c.gridx = 3;
-			c.gridy = 2;
-			connectionPanel.add(getConnectButton(), c);
-			
-			connectionPanel.setPreferredSize(new java.awt.Dimension(379, 121));
-		}
-		return connectionPanel;
-	}
-	
-	private JPanel getPluginPanel() {
-		if(pluginPanel == null) {
-			pluginPanel = new JPanel();
-			TitledBorder title = BorderFactory.createTitledBorder("Plugins");
-			pluginPanel.setBorder(title);
-		}
-		return pluginPanel;
-	}
-	
-	private JPanel getHolder(){
-		if(holder == null){
-			holder = new JPanel(new GridLayout(2,1));
-			holder.add(getConnectionPanel());
-			holder.add(getPluginPanel());
-		}
-		return holder;
-	}
-	
 	private JPanel getPanel(){
 		if(panel == null){
 			panel = new JPanel();
@@ -204,31 +122,5 @@ public class ClientView extends javax.swing.JFrame {
 			panel.add(getSendMessageButton());
 		}
 		return panel;
-	}
-	
-	/*Labels*/
-	private JLabel getIPLabel() {
-		if(IPLabel == null) {
-			IPLabel = new JLabel();
-			IPLabel.setText("IP-adress: ");
-			IPLabel.setPreferredSize(new java.awt.Dimension(60, 16));
-		}
-		return IPLabel;
-	}
-	
-	private JLabel getPortLabel() {
-		if(portLabel == null) {
-			portLabel = new JLabel();
-			portLabel.setText("Port: ");
-		}
-		return portLabel;
-	}
-	
-	private JLabel getAutoCon() {
-		if(autoCon == null) {
-			autoCon = new JLabel();
-			autoCon.setText(" Auto connect");
-		}
-		return autoCon;
 	}
 }
