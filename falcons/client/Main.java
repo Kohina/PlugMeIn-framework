970db43865becaf1a4796ff41da859008399c125
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-04-06 22:02:28 +0000

    Commiting the client Main

--- falcons/client/Main.java
+++ falcons/client/Main.java
@@ -1,33 +1,12 @@
 package falcons.client;
 
-import java.io.IOException;
-
-import falcons.client.network.ClientCommunicationCenter;
-import falcons.plugin.SendMessagePlugin;
-import falcons.plugin.manager.DataInterpreter;
-import falcons.plugin.manager.PluginModel;
-
 public class Main {
 
-	private static PluginModel pluginModel;
-	private static DataInterpreter interpreter;
-	private static int port = 45678;
-	private static SendMessagePlugin p = new SendMessagePlugin(); 
-	
-	/**
-	 * @param args
-	 */
-	public static void Main(String[] args) {
-		pluginModel = pluginModel.getInstance();
-		interpreter = interpreter.getInstance(true);
-		pluginModel.getPluginMap().put(p.getPluginID(), p);
-		Thread comThread;
-		try {
-			comThread = new Thread(new ClientCommunicationCenter(interpreter, "127.0.0.1", port));
-			comThread.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static Client client;
+
+	public static void main(String args[]) {
+		client = new Client();
+		client.run();
 	}
 
 }
