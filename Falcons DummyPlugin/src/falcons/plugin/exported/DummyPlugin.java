d51bb03ad21582872b50267ef43dba440e6a96f9
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-04-13 12:45:26 +0000

    Commiting the DummyPlugin again?

---
+++ Falcons DummyPlugin/src/falcons/plugin/exported/DummyPlugin.java
@@ -0,0 +1,24 @@
+package falcons.plugin.exported;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import falcons.plugin.AbstractPlugin;
+import falcons.plugin.Plugin;
+import falcons.plugin.PluginCall;
+
+@Plugin(pluginID = "Dummy Plugin", versionID = "0.1")
+public class DummyPlugin extends AbstractPlugin {
+
+	@Override
+	public void receiveCall(PluginCall call) {
+	}
+
+	@Override
+	public JPanel getMainPanel() {
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("IT WORKS"));
+		return panel;
+	}
+
+}
