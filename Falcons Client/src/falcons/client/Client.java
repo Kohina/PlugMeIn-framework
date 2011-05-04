762016bfb138e663f60841ffe688ea7c86661d2a
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-04 18:51:32 +0000

    Changed the Client so that it would still work after the changes made today.

--- Falcons Client/src/falcons/client/Client.java
+++ Falcons Client/src/falcons/client/Client.java
@@ -1,49 +1,34 @@
 package falcons.client;
 
-import java.awt.AWTException;
-import java.awt.Image;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
-import java.awt.SystemTray;
-import java.awt.Toolkit;
-import java.awt.TrayIcon;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Set;
-
-import javax.swing.JSeparator;
-
 import falcons.client.model.ClientPreferencesLogic;
+import falcons.client.model.PluginLogic;
+import falcons.client.network.ClientDataInterpreter;
 import falcons.client.view.ClientSystemTray;
 import falcons.client.view.ClientView;
 import falcons.client.view.ConnectionView;
-import falcons.pluginmanager.DataInterpreter;
-import falcons.pluginmanager.PluginModel;
 
 public class Client{
 	
 	private static ClientView clientView;
 	private static ConnectionView connectionView;
-	private static PluginModel pluginModel;
-	private static DataInterpreter interpreter;
 	private static ClientSystemTray systemTray;	
 	
 	public static final File DIR = new File(System.getProperty("user.dir"));
 
 	public void run(){
 		ClientPreferencesLogic.readPreferences();
-		pluginModel = PluginModel.getInstance();
-		interpreter = DataInterpreter.getInstance(true);
+		PluginLogic.loadPlugins();
+
 		clientView = new ClientView();
 		connectionView = new ConnectionView();
 		systemTray = new ClientSystemTray(clientView, connectionView);
 
-		Object[] nameSet = pluginModel.getPluginMap().keySet().toArray();
+		Object[] nameSet = PluginLogic.getPluginMap().keySet().toArray();
 		
 		for(Object o : nameSet){
 			String pluginName = o.toString();
-			clientView.addTab(pluginName, pluginModel.getPluginMap().get(pluginName).getMainPanel());
+			clientView.addTab(pluginName, PluginLogic.getPluginMap().get(pluginName).getMainPanel());
 		}
 	}
 
