fa80d9b608091f325e0b56ff3b95d2908d8fd015
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-04 18:51:08 +0000

    PluginLogic is now a purely static class. Also fixed a bug where we would never assign the loaded plugins to the plugin model.

--- Falcons Library/src/falcons/client/model/PluginLogic.java
+++ Falcons Library/src/falcons/client/model/PluginLogic.java
@@ -9,28 +9,13 @@ import falcons.pluginmanager.PluginLoader;
 
 public class PluginLogic {
 	
-	private static PluginLogic instance;
-	private PluginLoader pluginLoader = new PluginLoader();
-	
-	private PluginLogic(){
-		
-	}
-	
-	public static PluginLogic getInstance(){
-		if(instance == null){
-			return new PluginLogic();
-		}else{
-			return instance;
-		}
-	}
-	
-	public void loadPlugins(){
-		List<AbstractPlugin> pluginList = pluginLoader.loadPlugins();
+	public static void loadPlugins(){
+		List<AbstractPlugin> pluginList = PluginLoader.loadPlugins();
 		HashMap<String, AbstractPlugin> pluginMap = new HashMap<String, AbstractPlugin>();
 		for(AbstractPlugin p : pluginList){
 			pluginMap.put(p.getClass().getAnnotation(Plugin.class).pluginID(), p);
 		}
-		
+		PluginModel.setPluginMap(pluginMap);
 	}
 
 	/**
@@ -38,7 +23,7 @@ public class PluginLogic {
 	 * @return Returns a map containing all the currently loaded plugins with
 	 *         their pluginID as key.
 	 */
-	public HashMap<String, AbstractPlugin> getPluginMap() {
+	public static HashMap<String, AbstractPlugin> getPluginMap() {
 		return PluginModel.getPlugins();
 	}
 }
