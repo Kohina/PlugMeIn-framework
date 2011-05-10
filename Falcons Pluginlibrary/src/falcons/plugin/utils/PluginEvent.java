3cad7762d8a0983cbdae2e5d249e3502ecfa8a13
marre_kent@hotmail.com <marre_kent@hotmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-10 18:10:48 +0000

    PluginEvent updates, has comments!

--- Falcons Pluginlibrary/src/falcons/plugin/utils/PluginEvent.java
+++ Falcons Pluginlibrary/src/falcons/plugin/utils/PluginEvent.java
@@ -1,7 +1,28 @@
 package falcons.plugin.utils;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 //TODO Implement in the same type of way as ClientEvent, to be used by PluginEventListener
 // Should inclued commands such as SEND and GET_<DATA>.
+// Förmodligen helt fel, jag ger upp lite nu då jag inte riktigt förstår vad jag ska göra
 public class PluginEvent {
+	
+	private ObjectOutputStream out;
 
+	/**
+	 * Sends a PluginCall to the connected client. Takes the PluginCall as a
+	 * parameter.
+	 * 
+	 * @param call
+	 *            The PluginCall which should be sent to the client.
+	 * @throws IOException
+	 */
+	public void send(PluginCall call) {
+		try {
+			out.writeObject(call);
+		} catch (IOException e) {
+			System.err.print("Could not write to the output stream.");
+		}
+	}
 }
--- Falcons Pluginlibrary/src/falcons/plugin/utils/PluginEventListener.java
+++ Falcons Pluginlibrary/src/falcons/plugin/utils/PluginEventListener.java
@@ -3,6 +3,8 @@ package falcons.plugin.utils;
 import java.util.EventListener;
 
 // TODO: Should just expose the interface that PluginEventListener in falcons.client.pluginmanager will use.
-public interface PluginEventListener{
+// typ kanske fel här med???? :S
+public interface PluginEventListener extends EventListener{
 
+	public void actionPerformed(PluginEvent p);
 }
