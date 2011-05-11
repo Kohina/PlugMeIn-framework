2b243fe78fdf19b1120b9e8d6844cf3a9efc29d8
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-11 19:34:00 +0000

    Corrected ConnectionsModel.

--- Falcons Library/src/falcons/server/model/ConnectionModel.java
+++ Falcons Library/src/falcons/server/model/ConnectionModel.java
@@ -4,71 +4,82 @@ import java.util.*;
 
 import falcons.client.model.ServerLogic;
 import falcons.server.network.ConnectionThread;
+import falcons.utils.ClientInfo;
 
 public class ConnectionModel {
-	
+
 	private static ConnectionModel instance = new ConnectionModel();
-	private static List<ConnectionThread> connections = new ArrayList<ConnectionThread>();
-	
+	private static HashMap<ConnectionThread, ClientInfo> connections = new HashMap<ConnectionThread, ClientInfo>();
+
 	private ConnectionModel() {
-		//Do Nasing.
+		// Do Nasing.
 	}
-	
+
 	public static ConnectionModel getInstance() {
-			if(instance == null){
-				instance = new ConnectionModel();
-			}
+		if (instance == null) {
+			instance = new ConnectionModel();
+		}
 		return instance;
 	}
-	
+
 	/**
 	 * Gets the ConnectionThread with the given ID.
-	 * @param id The ID of the thread wanted.
-	 * @return Always check if the returned value is null, if it's null the thread was not found.
+	 * 
+	 * @param id
+	 *            The ID of the thread wanted.
+	 * @return Always check if the returned value is null, if it's null the
+	 *         thread was not found.
 	 */
-	public static ConnectionThread getConnection(Long id) {
-		boolean found = false;
-		for(int i = 0;i<connections.size()-1; i++){
-			if(connections.get(i).getId() == id){
-				return connections.get(i);
+	public ConnectionThread getConnection(long id) {
+		ConnectionThread returnObject = null;
+
+		ConnectionThread[] conns = (ConnectionThread[]) connections.keySet()
+				.toArray();
+
+		for (ConnectionThread c : conns) {
+			if (c.getId() == id) {
+				returnObject = c;
 			}
 		}
-		return null;
+		return returnObject;
 	}
-	
-	public Long getConnectionID(ConnectionThread thread) {
-		return connections.get(connections.lastIndexOf(thread)).getId();
-	}
-	
+
 	public void addConnection(ConnectionThread thread) {
-		connections.add(thread);
+		connections.put(thread, null);
 	}
-	
-	public void removeConnection(long id) {
-		for(int i = 0; i<connections.size()-1; i++){
-			if(connections.get(i).getId() == id){
-				connections.remove(i);
-			}
-		}
-	}
-	
+
 	public void removeConnection(ConnectionThread thread) {
 		connections.remove(thread);
 	}
-	
+
 	/**
 	 * @return Returns a list with all the currently active ConnectionThreads.
 	 */
-	public List<ConnectionThread> getConnectionList() {
-		// TODO Should be getConnection(), we don't want something to change the list. (Or perhaps return a deep copy of the list?)
-		return connections;
+	public HashMap<ConnectionThread, ClientInfo> getConnectionList() {
+		return (HashMap<ConnectionThread, ClientInfo>) connections.clone();
 	}
 
-	public static HashMap<String, String> getClientPlugins(long clientID) {
-		return ServerLogic.getPlugins(clientID);
+	public static HashMap<String, String> getClientPlugins(long id) {
+		ConnectionThread[] conns = (ConnectionThread[]) connections.keySet()
+				.toArray();
+		ConnectionThread key = null;
+
+		for (ConnectionThread c : conns) {
+			if (c.getId() == id) {
+				key = c;
+			}
+		}
+		return connections.get(key).getPlugins();
 	}
 
-	public static Set<ClientInfo> getClients() {
-		return ServerLogic.getClients();
+	public static List<ClientInfo> getClients() {
+		List<ClientInfo> clients = new ArrayList<ClientInfo>();
+		ConnectionThread[] conns = (ConnectionThread[]) connections.keySet()
+				.toArray();
+
+		for (ConnectionThread c : conns) {
+			clients.add(connections.get(c));
+		}
+		return clients;
 	}
 }
