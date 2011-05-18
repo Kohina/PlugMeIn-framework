418e1328289f8254c1b0a117a80daa6033dd7444
glimjaur@gmail.com <glimjaur@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-18 17:40:18 +0000

    Created TellstickLogic and modified TellstickModel, implemented a bunch of models.

--- Falcons TellstickPlugin/src/falcons/plugin/exported/tellstickPlugin/model/TellstickModel.java
+++ Falcons TellstickPlugin/src/falcons/plugin/exported/tellstickPlugin/model/TellstickModel.java
@@ -1,44 +1,86 @@
 package falcons.plugin.exported.tellstickPlugin.model;
 
-import java.io.Serizable;
-import java.util.Observable;
-import java.util.Observer;
+import java.io.Serializable;
+import java.util.*;
+import pkj.no.tellstick.device.*;
 
 import falcons.plugin.Pluggable;
 
 public class TellstickModel extends Observable implements Pluggable, Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	public  TellstickModel() {
 		// Nothing to do..
 	}
 	
 	public ArrayList<TellstickDevice> getDevices() {
-		return ArrayList<TellstickDevice> devices = TellstickDevice.getDevices();
+		ArrayList<TellstickDevice> devices = null;
+		try {
+			devices = TellstickDevice.getDevices();
+		} catch (SupportedMethodsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return devices;
 	}
 	
-	public void turnDevicesOn(ArrayList<TellstickDevice> devices) {
+	public void turnDevicesOn(ArrayList<TellstickDevice> devices) throws Exception {
 		for(TellstickDevice device : devices) {
 			if (device instanceof Device){
 				// Cast to device so we can get the method.
 				try{
 					((Device) device).on();
 				}catch(TellstickException e){
-					System.out.println(e.getMessage()); // Prints error right from the tellstick if we get error. Forexample if tellstick is not found this will print " Tellstick not found ". 
+					throw new Exception(e.getMessage());
 				}
 			}
 		}
 	}
 	
-	public void turnDevicesOff(ArrayList<TellstickDevice> devices) {
+	public void turnDeviceOn(TellstickDevice device) throws Exception {
+		if(device instanceof Device) {
+			try{
+				((Device) device).on();
+			}catch(TellstickException e) {
+				throw new Exception(e.getMessage());
+			}
+		}
+	}
+	
+	public void turnDevicesOff(ArrayList<TellstickDevice> devices) throws Exception {
 		for(TellstickDevice device : devices) {
-			if(device intanceof Device) {
+			if(device instanceof Device) {
 					try{
 						((Device) device).off();
 					}catch(TellstickException e){
-						System.out.println(e.getMessage());
+						throw new Exception(e.getMessage());
 					}
 				}
 			}
+		}
+	
+	public void turnDeviceOff(TellstickDevice device) throws Exception {
+		if(device instanceof Device) {
+			try{
+				((Device) device).off();
+			}catch(TellstickException e) {
+				throw new Exception(e.getMessage());
+			}
+		}
+	}
+	
+	public void dimDevice(TellstickDevice device, int level) throws Exception, IllegalArgumentException {
+		if (level > 0 || level < 256) {
+			if (device instanceof DimmableDevice) {
+				try {
+					((DimmableDevice) device).dim(level);
+				} catch (TellstickException e) {
+					throw new Exception(e.getMessage());
+				}
+			}
+		} else {
+			throw new IllegalArgumentException("Level has to be in the range 0-255");
 		}
 	}
 }
