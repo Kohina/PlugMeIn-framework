4e6385e47fd0fe8c0de89d1d3b6eb659a3ceac43
marre_kent@hotmail.com <marre_kent@hotmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-23 18:14:40 +0000

    Jag har tappat det

--- TicTacToePlugin/src/falcons/plugin/exported/ticTacToePlugin/view/TicTacToeMainPanel.java
+++ TicTacToePlugin/src/falcons/plugin/exported/ticTacToePlugin/view/TicTacToeMainPanel.java
@@ -38,34 +38,34 @@ public class TicTacToeMainPanel extends JPanel implements Observer, Pluggable, A
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == buttons[0]){
-			controller.played(0);
+			controller.turn(0, true);
 		}
 		else if(e.getSource() == buttons[1]){
-			controller.played(1);
+			controller.turn(1, true);
 		}
 		else if(e.getSource() == buttons[2]){
-			controller.played(2);
+			controller.turn(2, true);
 		}
 		else if(e.getSource() == buttons[3]){
-			controller.played(3);
+			controller.turn(3, true);
 		}
 		else if(e.getSource() == buttons[4]){
-			controller.played(4);
+			controller.turn(4, true);
 		}
 		else if(e.getSource() == buttons[5]){
-			controller.played(5);
+			controller.turn(5, true);
 		}
 		else if(e.getSource() == buttons[6]){
-			controller.played(6);
+			controller.turn(6, true);
 		}
 		else if(e.getSource() == buttons[7]){
-			controller.played(7);
+			controller.turn(7, true);
 		}
 		else if(e.getSource() == buttons[8]){
-			controller.played(8);
+			controller.turn(8, true);
 		}
 		else if(e.getSource() == buttons[9]){
-			controller.played(9);
+			controller.turn(9, true);
 		}
 		else{
 			System.out.print("Invalid button");
