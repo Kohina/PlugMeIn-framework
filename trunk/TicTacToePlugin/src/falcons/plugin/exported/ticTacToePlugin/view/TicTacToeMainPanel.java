12c6cb32ccc6157c1783ba6a9a44d3103dc9914b
marre_kent@hotmail.com <marre_kent@hotmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-23 15:33:17 +0000

    Updated GUI

--- trunk/TicTacToePlugin/src/falcons/plugin/exported/ticTacToePlugin/view/TicTacToeMainPanel.java
+++ trunk/TicTacToePlugin/src/falcons/plugin/exported/ticTacToePlugin/view/TicTacToeMainPanel.java
@@ -1,7 +1,35 @@
 package falcons.plugin.exported.ticTacToePlugin.view;
 
-import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.*;
+import falcons.plugin.Pluggable;
 
-public class TicTacToeMainPanel extends JPanel{
+public class TicTacToeMainPanel extends JPanel implements Observer, Pluggable, ActionListener{
 
+	private JButton buttons[] = new JButton[9];
+	
+	public TicTacToeMainPanel(){
+		initGUI();
+		this.setVisible(true);
+	}
+	
+	private void initGUI(){
+		this.setLayout(new GridLayout(3,3));
+		this.setSize(600, 600);
+		
+		for(int i=0; i<=8; i++){
+		    buttons[i] = new JButton();
+		    this.add(buttons[i]);
+		    buttons[i].addActionListener(this);
+		}
+	}
+	
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
