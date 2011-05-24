ea1ce8578f4208ba3c49d83913135aec05313138
julius.printz@gmail.com <julius.printz@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-24 10:02:11 +0000

    SendMessagePlugin is now complete.

--- Falcons SendMessagePlugin/src/falcons/plugin/exported/sendMessagePlugin/SendMessagePlugin.java
+++ Falcons SendMessagePlugin/src/falcons/plugin/exported/sendMessagePlugin/SendMessagePlugin.java
@@ -8,54 +8,31 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 import falcons.plugin.*;
+import falcons.plugin.exported.sendMessagePlugin.controller.SendMessageController;
 import falcons.plugin.exported.sendMessagePlugin.model.*;
 import falcons.plugin.exported.sendMessagePlugin.view.SendMessageMainPanel;
 
 @Plugin(pluginID = "Send Message", versionID = "0.1")
 public class SendMessagePlugin extends AbstractPlugin {
 
-	MessageModel messageModel;
-	MessageLogic messageLogic;
-	SendMessageMainPanel mainPanel;
+	private MessageModel messageModel;
+	private MessageLogic messageLogic;
+	private SendMessageMainPanel mainPanel;
+	private SendMessageController controller;
 
 	public SendMessagePlugin() {
 		messageModel = new MessageModel();
 		messageLogic = new MessageLogic(messageModel);
-		mainPanel = SendMessageMainPanel.getInstance(messageModel);
+		mainPanel = SendMessageMainPanel.getInstance(messageModel, controller);
+		controller = new SendMessageController(mainPanel, messageLogic);
 	}
-
-	// Comment the main-method when done with testing.
-	/*public static void main(String args[]) {
-		SendMessagePlugin p = new SendMessagePlugin();
-		JFrame frame = new JFrame();
-		frame.getContentPane().add(mainPanel);
-		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-	}*/
 
 	@Override
 	public void receiveCall(PluginCall call) {
-		AbstractPluginData data = call.getPluginData();
+		String data = (String) call.getPluginData().getData();
 
-		if (data.getMethodID().equals("SendMessage")
-				&& data.getVersionID()
-						.equals(this.getClass().getAnnotation(Plugin.class)
-								.versionID())) {
-			messageLogic.receiveMessage(((SendMessagePluginData) data).getMessage());
-		} else if (!data.getMethodID().equals("SendMessage")
-				&& !data.getVersionID()
-						.equals(this.getClass().getAnnotation(Plugin.class)
-								.versionID())) {
-			System.out
-					.println("Both the methodID and the versionID is not the same as the plugin sending the call.");
-		} else if (!data.getMethodID().equals("SendMessage")) {
-			System.out
-					.println("There doesn't exist a method with that methodID.");
-		} else if (!data.getVersionID().equals(
-				this.getClass().getAnnotation(Plugin.class).versionID())) {
-			System.out
-					.println("The version of the plugin sending the PluginCall is not the same as the one receiving it.");
+		if (call.getPluginData().getMethodID().equals("SendMessage")) {
+			controller.receiveMessage(data);
 		}
 
 	}
