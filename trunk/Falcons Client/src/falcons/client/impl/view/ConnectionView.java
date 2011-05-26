30ef496e3674b084bc00f7481566b681c4426128
glimjaur@gmail.com <glimjaur@gmail.com@ce0ce106-1b56-22f3-a529-550f69d82e3d>
2011-05-26 20:32:42 +0000

    Fixed an import in the Client

--- trunk/Falcons Client/src/falcons/client/impl/view/ConnectionView.java
+++ trunk/Falcons Client/src/falcons/client/impl/view/ConnectionView.java
@@ -12,6 +12,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
